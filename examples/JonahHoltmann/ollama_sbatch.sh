07e3d85b8ce763118321472cd584baa7c9d7f39d
Jonah Holtmann <84203424+katercross@users.noreply.github.com>
2025-10-02 13:46:08 +0200

    JH examples

---
+++ examples/JonahHoltmann/ollama_sbatch.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -l
+
+## This file is called `ollama_sbatch.sh`
+#SBATCH --job-name=ollama_test_server
+#SBATCH --time=00:20:00
+#SBATCH --qos=default
+#SBATCH --partition=gpu
+#SBATCH --account=p200981                      # Put your project ID
+#SBATCH --nodes=1                          # num of nodes
+#SBATCH --ntasks=1                         # number of tasks
+#SBATCH --ntasks-per-node=1                # number of tasks per node
+
+
+echo "Date              = $(date)"
+echo "Hostname          = $(hostname -s)"
+echo "Working Directory = $(pwd)"
+
+
+# Load the env
+module add Apptainer
+
+
+# Run the processing
+apptainer pull docker://ollama/ollama
+apptainer exec --nv ollama_latest.sif ollama serve
